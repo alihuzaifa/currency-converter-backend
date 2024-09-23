c6f86603898bb0f2a961339c156d1e96d90ef902
alihuzaifa <ha4587323@gmail.com>
2024-09-23 17:06:27 +0500

    add

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: "https://currency-converter-backend-gn9r.vercel.app",
+      url: "https://currency-converter-backend-six.vercel.app",
     },
   ],
   paths: { ...currencyDocumentation },
